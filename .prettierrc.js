7dd2af845cbab116eafc45a5972659a46c81e9cb
Aleks Hudochenkov <aleks@hudochenkov.com>
2019-12-01 14:57:22 +0100

    Setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// https://prettier.io/docs/en/options.html
+module.exports = {
+	arrowParens: 'always',
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 4,
+	useTabs: true,
+};
